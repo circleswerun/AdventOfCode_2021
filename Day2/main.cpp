7f1734bfd8a9a651d0a9c19b417379ec21eb620f
kylekellyj <kkelly93@gatech.edu>
2021-12-02 14:22:25 -0500

    Day2 chall1 complete

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::ifstream infile("D:\\AdventOfCode_2021\\Day2\\data.txt");
+
+    std::string direction;
+    int x = 0, y = 0;
+    int move;
+
+    while (infile >> direction >> move){
+        if(!infile.is_open()){
+            std::cerr << "file closed unexpectedly" << std::endl;
+        }
+        if(direction.compare("up") == 0){
+            y -= move;
+        }
+        else if(direction.compare("down") == 0){
+            y += move;
+        }
+        else if(direction.compare("forward") == 0){
+            x += move;
+        }
+        else{
+            std::cerr << "invalid direction" << std::endl;
+        }
+    }
+    infile.close();
+    std::cout << "final position of submarine: " << x << "," << y << std::endl;
+    std::cout << "final horizontal position multipled by final depth: " << x * y << std::endl;
 }
